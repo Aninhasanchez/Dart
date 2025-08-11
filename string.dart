bc9665930a0dfa02d6c2488b4f45fa3571a2e540
Ana Luisa Sanchez <49624570809@>
2025-08-11 16:34:22 -0300

    11-08

--- string.dart
+++ string.dart
@@ -4,6 +4,8 @@ main(){
   String primeira_string = "Olá, ";
   String Segunda_string = "Boa Tarde";
 
+  primeira_string = primeira_string.toUpperCase();
+
   print(primeira_string + Segunda_string);
 
 
