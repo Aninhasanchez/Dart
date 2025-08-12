40d8f55e35da148478584b1b86402d712d95666e
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-12 15:06:48 -0300

    12-08

---
+++ notacao_ponto.dart
@@ -0,0 +1,14 @@
+
+main(){
+//notação ponto é usado para acessar membros (propriedades ou metodos) de um objeto
+//exemplo usando a notaçã ponto para fazer um trucate no numero
+
+
+double valor_qualquer = 9.99.truncateToDouble();
+print(valor_qualquer);
+
+
+}
+
+
+
