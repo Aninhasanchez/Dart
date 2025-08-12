9d132b5f9f96810d59120a6419e3f18dbd265f07
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-12 14:27:35 -0300

    12-08

---
+++ map.dart
@@ -0,0 +1,13 @@
+main(){
+
+  //Map é tipo chave: valor
+  var sensores_medida = {
+    'Umidade': '%',
+    'Temperatura' : 'Graus Celsius',
+    'Contador' : 'Unidade', 
+    'Luminosidade' : 'Lux'
+  };
+
+print(sensores_medida);
+
+}
