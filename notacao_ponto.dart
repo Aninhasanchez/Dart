ab3ed66edb249e9a077023f91625da98c9e47a81
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-12 15:38:53 -0300

    12-08

--- notacao_ponto.dart
+++ notacao_ponto.dart
@@ -7,6 +7,16 @@ main(){
 double valor_qualquer = 9.99.truncateToDouble();
 print(valor_qualquer);
 
+//exemplo usando notação ponto para transformar um valor literal em tudo maiusculo
+print("Sorria e Seja feliz Sempre!!!".toUpperCase());
+
+//Usando a notação ponto para extrair um substring, ou seja, um pedaço de uma string
+String S1 = "Sorria e Seja feliz Sempre!!!";
+String S2 = S1.substring(0, 5); //o primeiro numero indica em qual caracter vou começar 
+                                //o segundo numero indica quantos caracteres pegar a partir do indicado
+
+print(S2);                        
+
 
 }
 
