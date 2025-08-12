b20ba97792577c67381bd391ab308e99fea6accf
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-12 13:50:22 -0300

    12-08

--- lista.dart
+++ lista.dart
@@ -15,7 +15,12 @@ main(){
 
 var tipos_sensores = ['Umidade', 'Temperatura', 'Contador', 'Luminosidade', 'Umidade do Solo'];
 
+// print(tipos_sensores);
+
 //Para saber o tamanho da lista
-print(tipos_sensores.length);
+// print(tipos_sensores.length);
+
+//Acessando os elementos da lista. Iternar o indice para ver o resultado
+print(tipos_sensores.elementAt(0));
 
 }
