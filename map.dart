acfc245f4cf2a8abb4aed27afeda7c22e417f5be
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-12 14:44:26 -0300

    12-08

--- map.dart
+++ map.dart
@@ -21,22 +21,34 @@ main(){
 // print(sensores_medida);
 
 //Mostrando que chave:valor não aceita repetição e caso ocorra fica valendo  o ultimo digitado
-var sensores_medida = {
+// var sensores_medida = {
+//   'Umidade': 80,
+//   'Temperatura' : 26,
+//   'Contador' : 1234,
+//   'Luminosidade' : 500,
+//   'Umidade' : 20
+// };
+// print(sensores_medida);
+
+// //Para saber o tamanho do MAP
+// print(sensores_medida.length);
+
+// // para acessar todas as chaves
+// print(sensores_medida.keys);
+
+// // para acessar todas as chaves
+// print(sensores_medida.values);
+
+//Mais uma indicação de que a linguagem dart é tipada
+//Ao usar o MAP eu posso especificar os tipos da chave e valor
+Map<String, double> sensores_medida_map = {
   'Umidade': 80,
   'Temperatura' : 26,
   'Contador' : 1234,
   'Luminosidade' : 500,
-  'Umidade' : 20
+
 };
-print(sensores_medida);
+print('$sensores_medida_map' + 'Sensores medidas map');
 
-//Para saber o tamanho do MAP
-print(sensores_medida.length);
-
-// para acessar todas as chaves
-print(sensores_medida.keys);
-
-// para acessar todas as chaves
-print(sensores_medida.values);
 
 }
