1b8b3cb1aec2e6d58f7a9a2a4effa6f1b63229b6
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-12 13:42:02 -0300

    12-08

---
+++ lista.dart
@@ -0,0 +1,14 @@
+// Criar lista usando colchetes
+
+main(){
+
+  List tipos_sensores = [
+    'Umidade',
+    'Temperatura',
+    'Contador',
+    'Luminosidade',
+    'Umidade do Solo'
+
+  ];
+
+}
