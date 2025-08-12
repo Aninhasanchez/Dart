ef0bcadedf8bc0e36649f3cfadf3d1f2dd3efb05
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-12 16:11:07 -0300

    12-08

--- operadores.dart
+++ operadores.dart
@@ -25,5 +25,15 @@ main(){
   print(45 % 3);
   print(47 % 2);
 
+  //resultado 27 pois faz b/a = 1,5 > 1,5 * 8 = 12 > 12 + 9 = 21 > 21 + a  = 27
+  print(a + b / a * 8 + 9);
+
+  // se % é usado para resto da divisao, como fazer um calculo de porcentagem?
+  // exemplo, preciso de uma conta que faça 34,5% de 1234
+  print(1234 / 100 * 34.5);
+
+  print(1940 / 100 * 8);
+  
+
 
 }
