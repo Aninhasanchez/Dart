f0f4b6055287f75259e497e265a9ba6736275a54
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-12 15:57:45 -0300

    12-08

---
+++ operadores.dart
@@ -0,0 +1,20 @@
+main(){
+
+  //operadores aritmeticos
+  // soma, subtração, divisão, multiplicação e resto
+  int a = 6;
+  int b = 9;
+
+  //soma
+  print(a + b);
+
+  //subtração
+  print(b - a);
+  print(a - b);
+
+  // divisão
+  print(a / b);
+  print(b / a);
+
+
+}
