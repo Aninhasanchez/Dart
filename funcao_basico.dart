d022f90c93306e9294f14fb68c7ffbd04239f05c
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-25 15:47:53 -0300

    25-08

---
+++ funcao_basico.dart
@@ -0,0 +1,14 @@
+main(){
+
+  exemploFuncaoSemParametro();
+
+}
+
+//criando uma função que não retorna nada
+//e não recebe parametro
+
+void exemploFuncaoSemParametro(){
+
+  print('Olá Muito Boa Tarde a Todos e Todas !!!');
+  
+}
