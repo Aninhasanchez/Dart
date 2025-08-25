cdd39e3ed5c80e0526519a235c4796503ecfb0a6
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-25 16:29:22 -0300

    25-08

--- exercicio_Funcao.dart
+++ exercicio_Funcao.dart
@@ -2,7 +2,36 @@
 main(){
 
 
-
-
-
+void saudar(String nome) {
+  print("Olá, $nome!");
 }
+
+
+//EXERCICIO 08
+int soma(int a, int b) {
+  return a + b;
+}
+
+
+//EXERCICIO 09
+int multiplica(int a, {int b = 2}) {
+  return a * b;
+}
+
+  // Chamando a função saudar
+  saudar("Ana");
+
+  // Chamando a função soma
+  int resultadoSoma = soma(10, 20);
+  print("Resultado da soma: $resultadoSoma");
+
+  // Chamando a função multiplica
+  int resultadoMultiplica1 = multiplica(5);
+  print("Multiplicando 5 (padrão b=2): $resultadoMultiplica1");
+
+  int resultadoMultiplica2 = multiplica(5, b: 3);
+  print("Multiplicando 5 x 3: $resultadoMultiplica2");
+}
+
+
+
