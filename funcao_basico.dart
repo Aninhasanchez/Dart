26ce886b9330fdb9aa9f4b06b4f41a9a8e723296
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-25 16:13:06 -0300

    25-08

--- funcao_basico.dart
+++ funcao_basico.dart
@@ -1,15 +1,22 @@
 main(){
+
+  //Variaveis declaradas e usadas na função 02
   DateTime now = DateTime.now();
   var a = "Hoje é dia";
   int diaDoMes = now.day;
 
+  //Chamando função 01
   // exemploFuncaoSemParametro();
+  
+  //Chamando função 02
+  //  exemploFuncaoComParametro(a, diaDoMes);
 
- exemploFuncaoComParametro(a, diaDoMes);
+  //Chamando função 03
+  print(exemploRetornaResultadoConta(2, 4, 6));
 
 }
 
-//criando uma função que não retorna nada
+//Função 01 - criando uma função que não retorna nada
 //e não recebe parametro
 void exemploFuncaoSemParametro(){
 
@@ -17,10 +24,20 @@ void exemploFuncaoSemParametro(){
 
 }
 
-//Criando função que recebe parametro
+//Função 02 - Criando função que recebe parametro
 //e nao retorna nada. Apenas executa
 void exemploFuncaoComParametro(String a, var b){
 
   print('$a $b');
 
+}
+
+//Função 03 - Criando função que retorna alguma coisa
+//int/double/string indica o tipo que vai ser retornado, depois vem o nome da função
+// e entre os parenteses os parametros que serao recebido
+int exemploRetornaResultadoConta(int a, int b, int c){
+
+  return(a * b * c);
+
+  
 }
