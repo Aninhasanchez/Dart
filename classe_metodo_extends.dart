b3f07d0a1579192330fa3d1c75800c37bc3f8913
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-26 14:23:10 -0300

    26-08

--- classe_metodo_extends.dart
+++ classe_metodo_extends.dart
@@ -25,50 +25,81 @@ class DataHora extends Data {
 
   void obterDataFormatada(){
     print("$dia/$mes/$ano $hora:$minuto/$segundo");
-
   }
-  
+
+  String obterDataFormatadaComString(){
+    return "$dia/$mes/$ano $hora:$minuto:$segundo";
+  }
+
 }
 
 
 
 
 
-//Chamando a classe Data
+//Chamando as classes Data e DataHora
 main(){
 
-  var dataAtivacao = new Data();
+
+  //instanciando a classe Data
+  Data dataAtivacao = Data();
 
   dataAtivacao.dia = 10;
   dataAtivacao.mes = 4;
   dataAtivacao.ano = 2025;
 
-  print("Data de Ativação do sensor: ${dataAtivacao.dia}/${dataAtivacao.mes}/${dataAtivacao.ano}");
+  //Instanciando a classe DataHora
+  DataHora dataHoraLeitura = DataHora();
+
+  dataHoraLeitura.dia = 22;
+  dataHoraLeitura.mes = 5;
+  dataHoraLeitura.ano = 2025;
+  dataHoraLeitura.hora = 14;
+  dataHoraLeitura.minuto = 30;
+  dataHoraLeitura.segundo = 45;
+
+  //Exibindo os resultados
+  String d1 = dataAtivacao.obterDataFormatadaComString();
+  String d2 = dataHoraLeitura.obterDataFormatadaComString();
+
+  print("Data de ativação do sensor: $d1");
+  print("Data de leitura do sensor: $d2");
+
+    //Mostrando que a chamada mostra uma instacia do objeto
+  print(dataAtivacao);
+  print(dataHoraLeitura);
+  
+
+
+  // var dataAtivacao = new Data();
+
+  // dataAtivacao.dia = 10;
+  // dataAtivacao.mes = 4;
+  // dataAtivacao.ano = 2025;
+
+  // print("Data de Ativação do sensor: ${dataAtivacao.dia}/${dataAtivacao.mes}/${dataAtivacao.ano}");
 
   //Outro exemplo de como instaciar a mesma Classe uma segunda vez no código
   //Não é necessário colocar o new e no lugar de var posso colocar diretamente o nome da classe
 
-  Data dataLeitura = Data();
+  // Data dataLeitura = Data();
 
-  dataLeitura.dia = 22;
-  dataLeitura.mes = 4;
-  dataLeitura.ano = 2025;
+  // dataLeitura.dia = 22;
+  // dataLeitura.mes = 4;
+  // dataLeitura.ano = 2025;
 
-  print("Data da ultima Leitura do Sensor: ${dataLeitura.dia}/${dataLeitura.mes}/${dataLeitura.ano}");
+  // print("Data da ultima Leitura do Sensor: ${dataLeitura.dia}/${dataLeitura.mes}/${dataLeitura.ano}");
 
   //Acessando o metodo(Função) criando na classe
   // dataAtivacao.obterDataFormatada();
   // dataLeitura.obterDataFormatada();
 
   //acessando o metodo que retorna uma string de data formatada
-  String d1 = dataAtivacao.obterDataFormatadaComString();
-  String d2 = dataLeitura.obterDataFormatadaComString();
-  print("Data de ativação do sensor: $d1");
-  print("Data de leitura do sensor: $d2");
+  // String d1 = dataAtivacao.obterDataFormatadaComString();
+  // String d2 = dataLeitura.obterDataFormatadaComString();
+  // print("Data de ativação do sensor: $d1");
+  // print("Data de leitura do sensor: $d2");
 
-  //Mostrando que a chamada mostra uma instacia do objeto
-  print(dataAtivacao);
-  print(dataLeitura);
-  
+
 
 }
