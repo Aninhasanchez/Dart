688cf3ddee8de3f0e89767827a85fcca3707c6f4
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-26 13:38:33 -0300

    26-08

---
+++ classe_atributo.dart
@@ -0,0 +1,20 @@
+class Data {
+  //Atributos da classe
+  int dia = 1;
+  int mes = 1;
+  int ano = 1980;
+
+}
+
+//Chamando a classe Data
+main(){
+
+  var dataAtivacao = new Data();
+
+  dataAtivacao.dia = 10;
+  dataAtivacao.mes = 4;
+  dataAtivacao.ano = 2025;
+
+  print("Data de Ativação do sensor: ${dataAtivacao.dia}/${dataAtivacao.mes}/${dataAtivacao.ano}");
+  
+}
