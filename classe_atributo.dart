08ee30c8ac046ba830feb60db5768083b27749da
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-26 13:42:43 -0300

    26-08

--- classe_atributo.dart
+++ classe_atributo.dart
@@ -16,5 +16,17 @@ main(){
   dataAtivacao.ano = 2025;
 
   print("Data de Ativação do sensor: ${dataAtivacao.dia}/${dataAtivacao.mes}/${dataAtivacao.ano}");
+
+  //Outro exemplo de como instaciar a mesma Classe uma segunda vez no código
+  //Não é necessário colocar o new e no lugar de var posso colocar diretamente o nome da classe
+
+  Data dataLeitura = Data();
+
+  dataLeitura.dia = 22;
+  dataLeitura.mes = 4;
+  dataLeitura.ano = 2025;
+
+  print("Data da ultima Leitura do Sensor: ${dataLeitura.dia}/${dataLeitura.mes}/${dataLeitura.ano}");
   
+
 }
